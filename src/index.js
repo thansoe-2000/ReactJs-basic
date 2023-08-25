221b3265830131e3d5b5e8c52ef8553527e506d5
Kyaw Soe Hla <codewarrior.ksh@gmail.com>
2023-08-25 14:02:38 +0630

    'react-3'

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Greeting() {
-  return <h1>This is me</h1>; // JSX
+  return (
+    <div>
+
+    <h1>This is me, Nice to meet You React </h1>
+      <p>This is paragraph</p>
+      <a href="">Read More</a>
+  </div>
+  ); // JSX
 }
+
 
 ReactDOM.render(<Greeting />, document.getElementById('root'));
